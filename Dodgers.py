204d50934c1482d40d2c5ef68640f50b5f1bb09c
martajhill <hillmarta18@gmail.com>
2021-11-15 11:33:02 -0500

    pulled in data

--- Dodgers.py
+++ Dodgers.py
@@ -9,3 +9,25 @@ Created on Sun Nov 14 15:51:29 2021
 """
 dodgers file
 """
+
+from TeamClass import Team
+from PlayerClass import Player
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    dodgers15 = Team('LAN', '2015')
+    dodgers16 = Team('LAN', '2016')
+    dodgers17 = Team('LAN', '2017')
+    dodgers18 = Team('LAN', '2018')
+    dodgers19 = Team('LAN', '2019')
+
+    teams = [dodgers15, dodgers16, dodgers17, dodgers18, dodgers19]
+
+    win_percent = []
+
+    for team in teams:
+        win_percent.append(team.return_wins())
+
+    plt.bar([1,2,3,4,5], win_percent)    
+    
