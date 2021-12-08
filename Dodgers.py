85189cccbb2cd6a910b9d9b1bfceaca9a30804c2
martajhill <hillmarta18@gmail.com>
2021-12-08 11:24:03 -0500

    cleaned up dodgers file

--- Dodgers.py
+++ Dodgers.py
@@ -23,6 +23,11 @@ pd.set_option('display.max_columns', None)
 
 
 def plot_stats(team_df):
+    """ 
+    name: plot_stats
+    parameters: team_df, a data frame with all the team data
+    returns: none, a plot 
+    """
     """ Plots the 4 stats we are using on 4 different scatterplots with
     regular season and post season differentiated by color """
     for i in range(0, len(team_df.columns), 2):
@@ -32,6 +37,11 @@ def plot_stats(team_df):
 
 
 def calculate_team_stats(teams):
+    """ 
+    name: calculate_team_stats
+    parameters: teams, a list of the years of teams
+    returns: a nested dictionary with relevent statistics
+    """    
     """ Calculates the stats we are using for regular season and post season.
     Returns a dictionary with name of the stats as keys and the stat value as values """
     teams_stats = {}
@@ -51,14 +61,40 @@ def calculate_team_stats(teams):
                                   'Post_HRA_avg': pitch_post_hra}
     return teams_stats
 
+def get_team_stats(teams):
+    """
+    name: get_team_stats
+    parameters: teams, a list 
+    returns: a differently formatted dictionary of data
+    """
+    # Program intakes list of teams and gets all stats for the teams
+
+    years = {}
+
+    for team in teams:
+
+        years[team.id + team.year] = {"reg_batavg": team.reg_ba(), "post_batavg": team.post_ba(), "reg_era": team.reg_era(),
+                            "post_era": team.post_era(), "reg_hra": team.reg_hra(), "post_hra": team.post_hra(),
+                            "pitch_reg_hra": team.pitch_reg_hra(), "pitch_post_hra": team.pitch_post_hra()}
+    return years
 
 def plot_summary(stats_df):
+    """ 
+    name: plot_summary
+    parameters: stats_df, a data frame
+    returns: none, a plot
+    """
     """ Scatterplot of the summary statistics for the team across different years """
     sns.lineplot(data=stats_df)
     plt.show()
 
 
 def normalize(df):
+    """ 
+    name: normalize
+    paramater: df, a data frame
+    returns: df, a data frame with normalized values
+    """
     """ Scales all of the columns in the dataframe to values between 0 and 1 """
     index = df.index
     cols = df.columns
@@ -85,49 +121,8 @@ if __name__ == "__main__":
     2018: 92-71, 0.564, lost in world series to Red Sox
     2019: 106-56, 0.654, lost in NL series to Nationals
     """
-    """
-    win_percent = []
-    ranks = []
-    for team in teams:
-        win_percent.append(team.return_wins())
-        ranks.append(team.return_rank())
-
-    #pretty boring bc their win percent only ranges from 0.55 to like 0.65
-    plt.bar([1,2,3,4,5], win_percent)    
-    plt.figure(2)
-    
-    #they were ranked first every single one of these years
-   
-    batting = []
-    for team in teams:
-        year = []
-        for playerid, player in team.players.items():
-            try:
-                bat = player.return_bat_hr('2016')
-            except:
-                  pass
-            year.append(bat)
-        batting.append(year)
-        
-    print(batting[0])
-    plt.figure(3)
-    plt.scatter(range(len(batting[0])), batting[0])
-    plt.show()        
-    
-    #find averages for batting for each year
-    #ngl kind interesting bc 2015 has a much higher value than the other years
-    #[6.4, 3.4363636363636365, 4.365384615384615, 3.8076923076923075, 4.3478260869565215]
-    
-    avgs = []
-    for year in batting:
-        avg = sum(year)/len(year)
-        avgs.append(avg)
-    #print(avgs)
-    #stole the calculate_team_stats function from Astros and used it on dodgers        
-   # stats = calculate_team_stats(teams)
-    #print(stats)
-    
-    """
+    # make a csv wtih just Dodgers, commented out so it runs faster and uses
+    #the new csv not the big one
     """
     teams_stats = calculate_team_stats(teams)
     dodgers = pd.DataFrame(teams_stats)
@@ -143,7 +138,6 @@ if __name__ == "__main__":
     dodgers['Reg_Rank'] = [5, 8, 2, 2, 1]
     dodgers['Post_Round'] = [2, np.NaN, 5, 3, 4]
     """
-    #code below is adapted from Astros 
 
     # reading from csv to make the process faster
     dodgers = pd.read_csv('Dodgers.csv', index_col=0)
@@ -163,20 +157,18 @@ if __name__ == "__main__":
 
     # recombining the dataframes and adding inverse rank based on winning percentage/standings
     dodgers_reg = pd.concat([dodgers_reg_bat, dodgers_reg_pitch], axis=1)
-    #UPDATE NUMBERS, overall finish in MLB
+    #Uupdated numbers below based on overall finish in MLB
     #2015 6th 
     #2016 6th
     #2017 1st
     #2018 7th
     #2019 2nd
     dodgers_reg['Reg_Rank'] = [0.166, 0.166, 1, 0.142, 0.5]
-    #dodgers_reg['Reg_Rank'] = [0.2, 0.125, 0.5, 0.5, 1]
-
+    
     # normalizing all of the values
     dodgers_reg_scaled = normalize(dodgers_reg)
 
     # predicting the standing of the team based on the 4 stats (numbers chosen based on the visualization)
-    #TEXT PAXTON TO FIGURE OUT HOW TO FIX THIS
     dodgers_reg_scaled['Pred_Rank'] = dodgers_reg_scaled['Reg_BA'] * 0.1 + dodgers_reg_scaled['Reg_HR_avg'] * 0.4 + \
                                     dodgers_reg_scaled['Reg_ERA'] * 0.4 + dodgers_reg_scaled['Reg_HRA_avg'] * 0.1
 
@@ -191,13 +183,12 @@ if __name__ == "__main__":
     dodgers_post_pitch = dodgers_post_pitch.apply(lambda x: 1 / x)
     dodgers_post = pd.concat([dodgers_post_bat, dodgers_post_pitch], axis=1)
 
-    # instead of rank, I used round to represent the round they lost in the playoffs (or 5 if they won world series)
+    # instead of rank, we used round to represent the round they lost in the playoffs (or 5 if they won world series)
     dodgers_post['Post_Round'] = [2, np.NaN, 5, 3, 4]
 
     dodgers_post_scaled = normalize(dodgers_post)
 
     # again trying to predict post season success based off the 4 stats (numbers chosen based off visualization)
-    #WHERE DID THE NUMBERS COME FROM? CURIOUS AS TO METHODS?
     dodgers_post_scaled['Pred_Round'] = dodgers_post_scaled['Post_BA'] * 0.1 + dodgers_post_scaled['Post_HR_avg'] * 0.4 + \
                                      dodgers_post_scaled['Post_ERA'] * 0.4 + dodgers_post_scaled['Post_HRA_avg'] * 0.1
 
@@ -211,11 +202,5 @@ if __name__ == "__main__":
 
     print(dodgers_overall)
 
-    plot_summary(dodgers_overall)
-
-
-
-#under perfromed 15, 17, 19
-#over performed 16 and 18
 
 
